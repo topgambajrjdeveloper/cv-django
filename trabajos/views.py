8af57e2bdeaf140d34b38c4b8e914c3da575588e
julianjr <topgambajrjdeveloper@gmail.com>
2021-04-11 18:40:43 +0200

    models, urls, views, admin de trabajos

--- trabajos/views.py
+++ trabajos/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Trabajos, Categoria
+from django.views.generic import DetailView
 # Create your views here.
+
+
+def Jobs(request):
+    job = Trabajos.objects.all()
+    return render(request, 'trabajos.html', {'job': job})
+
+
+def Categorias(request, categoria_id):
+    categoria = Categoria.objects.get(id=categoria_id)
+    job = Trabajos.objects.filter(categorias=categoria)
+    return render(request, 'categorias.html', {'categoria': categoria, 'job': job})
+
+
+class JobsDetalView(DetailView):
+    model = Trabajos
+    template_name = 'detail.html'
